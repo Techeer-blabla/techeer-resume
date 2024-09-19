4adc6f5eaab17d50afb02bd2d43ff053e1e6e440
kim1387 <kevink0@naver.com>
2024-09-19 23:46:09 +0900

    refactor: main page rollback

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,14 +1,11 @@
-// src/pages/MainPage.tsx
-import React from 'react';
+import React from "react";
 
-const MainPage: React.FC = () => {
+function MainPage() {
     return (
-        <div className="flex flex-col items-center">
-            <h2 className="text-xl font-semibold mb-4">메인 페이지</h2>
-            {/* 메인 페이지 내용을 추가할 수 있습니다. */}
-            <p>여기에 메인 콘텐츠를 추가하세요.</p>
-        </div>
+        <>
+            <p>Hi</p>
+        </>
     );
-};
+}
 
 export default MainPage;
