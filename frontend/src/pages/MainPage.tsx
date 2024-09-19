4e8ddc1746c838198e321542af5f8c968d9a2a2d
jung2941 <ujin2941@naver.com>
2024-09-19 21:13:17 +0900

    design: 네비게이션 바 구성

---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <>
+      <p>Hi</p>
+    </>
+  );
+}
+
+export default MainPage;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      width: {
+        // 22: "5.5rem",
+        // 23: "5.8rem",
+        // 29: "7.25rem",
+        // 50: "12.5rem",
+        // 70: "17.5rem",
+        // 96: "24rem",
+        // 108: "27rem",
+        // 112: "28rem",
+        // 125: "31.25rem",
+        140: "35rem",
+      },
       colors: {
         primary: {
           DEFAULT: "#2bca43",
