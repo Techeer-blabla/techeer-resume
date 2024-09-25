be7b00cd704d22ee15eecf6d69c4440fb1bb51f3
gwanghyeon <154852665+Gwanghyeon-k@users.noreply.github.com>
2024-09-25 22:05:58 +0900

    feat: 이력서 조회, 태그 조회 api (#58)
    Co-authored-by: Gwanghyeon-k <hsys06207@gmail.com>

--- backend/src/main/java/com/techeer/backend/domain/resume/repository/ResumeRepository.java
+++ backend/src/main/java/com/techeer/backend/domain/resume/repository/ResumeRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface ResumeRepository extends PagingAndSortingRepository<Resume, Long>, JpaRepository<Resume, Long> {
 
+    List<Resume> findByUserUsername(String username);
+    List<Resume> findByPosition(Resume.Position position);
+    List<Resume> findByTechStack(Resume.TechStack techStack);
 }
