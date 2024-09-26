68bcd7dd5e5d49a762543477d3752229a147f48a
sanghun <vitash1215@gmail.com>
2024-09-26 16:40:41 +0900

    fix : 불필요한 컬럼 없애고 techStack은 다대다 관계로 매핑

---
+++ backend/src/main/java/com/techeer/backend/domain/resume/entity/TechStack.java
@@ -0,0 +1,28 @@
+package com.techeer.backend.domain.resume.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TechStack {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "techStack")
+    private List<ResumeTechStack> resumeTechStacks = new ArrayList<>(); ;
+
+    public TechStack(String name) {
+        this.name = name;
+    }
+}
