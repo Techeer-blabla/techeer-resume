6fc83aa13c04d45b3963ba0658448f332e27b1e0
sanghun <vitash1215@gmail.com>
2024-09-26 16:39:03 +0900

    fix : User 관련 file 주석처리, resume, user 테이블 매핑 관계 없앰

--- backend/src/main/java/com/techeer/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/techeer/backend/domain/user/service/UserService.java
@@ -1,22 +1,22 @@
-package com.techeer.backend.domain.user.service;
-
-import com.techeer.backend.domain.user.entity.User;
-import com.techeer.backend.domain.user.repository.UserRepository;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    // 다시 정해됨
-    public Optional<User> findUserByName(String userName) {
-        return userRepository.findByUsername(userName);
-    }
-
-}
+//package com.techeer.backend.domain.user.service;
+//
+//import com.techeer.backend.domain.user.entity.User;
+//import com.techeer.backend.domain.user.repository.UserRepository;
+//import lombok.AccessLevel;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+//public class UserService {
+//
+//    private final UserRepository userRepository;
+//
+//    // 다시 정해됨
+//    public Optional<User> findUserByName(String userName) {
+//        return userRepository.findByUsername(userName);
+//    }
+//
+//}
