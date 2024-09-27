25337f049dae5eec52706a8366b4ec99b6cb832c
박유경 <165669386+ukongee@users.noreply.github.com>
2024-09-27 13:57:48 +0900

    [feat] 이력서 등록페이지 퍼블리싱 완료 (#44)
    * design: 중간 커밋
    
    * 중간 커밋
    
    * 퍼블리싱 완료
    
    * 슬라이드 지우기
    
    * 슬라이더 수정
    
    * chore: auto-format code [skip ci]
    
    * 순서변경
    
    * 직접입력 태그 수정 완료
    
    * 수정
    
    * yarnlock 파일 삭제
    
    * yarn파일삭제
    
    * 파일 복구
    
    * yarn.lock frontend파일 생성
    
    * 파일 재삭제
    
    ---------
    
    Co-authored-by: parkyugyeong <qkrb8019@gmai.com>
    Co-authored-by: GitHub Action <action@github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import MainPage from "./pages/MainPage";
 import ResumeFeedbackPage from "./pages/ResumeFeedbackPage";
+import Upload from "./pages/Upload";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="flex-grow">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/resume-feedback" element={<ResumeFeedbackPage />} />
-        </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/resume-feedback" element={<ResumeFeedbackPage />} />
+            <Route path="/upload" element={<Upload />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
