f0e73ad82057d5edf9f23d171dde5a3708498913
juwon <67044438+YunJuwon0825@users.noreply.github.com>
2024-09-28 19:40:46 +0900

    [feat] spring security를 이용한 회원가입 (#71)
    * feat : 회원가입 구현
    
    * refactor : docker compose 정상 복귀
    
    * refactor : 유저이름은 중복 가능 , email이 ID 역할

--- backend/src/main/java/com/techeer/backend/api/user/controller/UserController.java
+++ backend/src/main/java/com/techeer/backend/api/user/controller/UserController.java
@@ -1,13 +1,35 @@
-//package com.techeer.backend.domain.user.controller;
-//
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@Tag(name = "User", description = "User API")
-//@RequestMapping("/api/v1")
-//public class UserController {
-//    // todo 테스트 용
-//
-//}
+package com.techeer.backend.api.user.controller;
+import com.techeer.backend.api.user.domain.User;
+import com.techeer.backend.api.user.dto.request.UserRegisterRequest;
+import com.techeer.backend.api.user.service.UserService;
+import com.techeer.backend.global.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class UserController {
+    private final UserService userService;
+
+    // todo String -> successResponse 로 변경
+    @Operation(summary = "회원가입")
+    @PostMapping(value= "/register")//, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> register(@RequestBody UserRegisterRequest req) {
+
+        if (userService.userExists(req.getEmail())) {
+            return new ResponseEntity<>("email already exists", HttpStatus.BAD_REQUEST); // 중복 검사
+        }
+
+        userService.register(req); // 사용자 등록
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED); // 등록 성공 응답
+    }
+}
--- backend/src/main/java/com/techeer/backend/api/user/domain/User.java
+++ backend/src/main/java/com/techeer/backend/api/user/domain/User.java
@@ -1,36 +1,50 @@
-//package com.techeer.backend.domain.user.entity;
-//
-//
-//import com.techeer.backend.global.common.BaseEntity;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//
-//@Getter
-//@Entity
-//@Table(name = "User")
-//public class User extends BaseEntity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "user_id")
-//    private Long id;
-//
-//    @Column(name = "username")
-//    private String username;
-//
-//    @Column(name = "password")
-//    private String password;
-//
-//    @Column(name = "email")
-//    private String email;
-//
-//    @Column(name = "refresh_token")
-//    private String refreshToken;
-//
+package com.techeer.backend.api.user.domain;
+
+
+import com.techeer.backend.api.user.dto.request.UserRegisterRequest;
+import com.techeer.backend.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "User")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
 //    @Column(name = "profile_image")
 //    private String profileImage;
-//
+
 //    @Enumerated(EnumType.STRING)
 //    @Column(name = "role")
 //    private Role role;
-//
-//}
+
+    private User(UserRegisterRequest req, String encodePW){
+        this.email = req.getEmail();
+        this.username = req.getUsername();
+        this.password = encodePW;
+        this.refreshToken = null;
+    }
+
+    public static User from(UserRegisterRequest req, String encodePW){
+        return new User(req, encodePW);
+    }
+}
