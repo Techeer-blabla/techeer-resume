1c77d592e04baf910ce2233d381d2be52b2ed4e2
yunjuwon <a77265192@gmail.com>
2024-09-28 16:53:37 +0900

    feat : 회원가입 구현

--- backend/src/main/java/com/techeer/backend/api/user/service/UserService.java
+++ backend/src/main/java/com/techeer/backend/api/user/service/UserService.java
@@ -1,22 +1,39 @@
-//package com.techeer.backend.domain.user.service;
-//
-//import com.techeer.backend.domain.user.entity.User;
-//import com.techeer.backend.domain.user.repository.UserRepository;
-//import lombok.AccessLevel;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//@Service
-//@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-//public class UserService {
-//
-//    private final UserRepository userRepository;
-//
-//    // 다시 정해됨
-//    public Optional<User> findUserByName(String userName) {
-//        return userRepository.findByUsername(userName);
-//    }
-//
-//}
+package com.techeer.backend.api.user.service;
+
+
+import com.techeer.backend.api.user.domain.User;
+import com.techeer.backend.api.user.dto.request.UserRegisterRequest;
+import com.techeer.backend.api.user.repository.UserRepository;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+    // 다시 정해됨
+    public Optional<User> findUserByName(String userName) {
+        return userRepository.findByUsername(userName);
+    }
+
+    public boolean userExists(@NotBlank String userName, @NotBlank String email) {
+        return userRepository.findByUsername(userName).isPresent() || userRepository.findByEmail(email).isPresent();
+    }
+
+    public void register(UserRegisterRequest req) {
+        // 비밀번호 암호화
+        String encodePW = (passwordEncoder.encode(req.getPassword()));
+        User user = User.from(req, encodePW);
+        userRepository.save(user);
+    }
+}
