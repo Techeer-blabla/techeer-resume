d9217c21741ef2922810332dde7f2acc00ae199c
yunjuwon <a77265192@gmail.com>
2024-10-01 23:14:01 +0900

    feat : 빌드 설정

--- backend/src/main/java/com/techeer/backend/api/user/config/SecurityConfig.java
+++ backend/src/main/java/com/techeer/backend/api/user/config/SecurityConfig.java
@@ -1,34 +1,93 @@
 package com.techeer.backend.api.user.config;
 
+import com.techeer.backend.api.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable) // 비활성화
-            .authorizeHttpRequests(authorize -> authorize
-                            .anyRequest().permitAll() //모든 접근 가능
-                    //.requestMatchers("/api/v1/login").permitAll()   // 2. api/v1/login 경로는 인증 없이 허용
-                    //.requestMatchers("/api/v1/register").permitAll() // api/v1/register 경로는 인증 없이 허용
-                    //.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger UI와 API docs 허용
-                    //.anyRequest().authenticated() // 3. 그 외의 모든 요청은 인증 필요
-            )
-            .httpBasic(Customizer.withDefaults()) // 4. HTTP Basic 인증
-            .formLogin(Customizer.withDefaults()); // 5. 기본 폼 로그인 사용
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .httpBasic(HttpBasicConfigurer::disable)
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/",
+                                "/oauth2/**",
+                                "/index.html",
+                                "/ws/**",
+                                "/swagger/**",
+                                "swagger-ui/**",
+                                "/api-docs/**",
+                                "/signup.html",
+                                "/api/v1/reissue"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(endpoint -> endpoint
+                                .userService(customOAuth2UserService))
+                        .successHandler(oAuth2LoginSuccessHandler) // 2.
+                        .failureHandler(oAuth2LoginFailureHandler) // 3.
+                )
+                .exceptionHandling(authenticationManager ->authenticationManager
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtService, userRepository), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:9100",
+                "http://localhost:9090",
+                "http://jxy.me",
+                "http://apic.app",
+                "file://",
+                "https://jiangxy.github.io",
+                "http://localhost"
+                //"https://rexume.site",
+                //"http://rexume.site"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
     @Bean
