a556f2ef7166b758d118e74f4b007ab96ebce3ff
yunjuwon <a77265192@gmail.com>
2024-10-12 13:33:17 +0900

    feat : cors에러 문제

--- backend/src/main/java/com/techeer/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/techeer/backend/global/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.techeer.backend.global.config;
 
 import java.util.List;
 
+import com.techeer.backend.global.jwt.JwtAuthenticationFilter;
+import com.techeer.backend.global.jwt.service.JwtService;
+import io.jsonwebtoken.Jwt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +14,10 @@ import org.springframework.security.config.annotation.web.configurers.HttpBasicC
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -32,6 +38,23 @@ public class SecurityConfig {
 	private final CustomOAuth2UserService customOAuth2UserService;
 	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+	private final JwtService jwtService;
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(List.of("*"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTION$S"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setExposedHeaders(List.of("*"));
+
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -42,66 +65,48 @@ public class SecurityConfig {
 			.sessionManagement(configurer -> configurer
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			)
-			.authorizeHttpRequests(requests ->
-				requests.anyRequest().permitAll() // 모든 요청을 모든 사용자에게 허용
+//			.authorizeHttpRequests(requests ->
+//					requests.anyRequest().permitAll() // 모든 요청을 모든 사용자에게 허용
+//			)
+			.authorizeHttpRequests(authorize -> authorize
+					.requestMatchers(
+							"/v3/api-docs/**",
+							"/oauth2/**",
+							"/oauth2/authorization/google",
+							"/index.html",
+							"/swagger/**",
+							"/swagger-ui/**",
+							"/swagger-ui/index.html/**",
+							"/api-docs/**",
+							"/signup.html",
+							"/api/v1/reissue"
+					).permitAll()
+					.anyRequest().authenticated()
 			)
-			//                .authorizeHttpRequests(authorize -> authorize
-			//                        .requestMatchers(
-			//                                "/",
-			//                                "/oauth2/**",
-			//                                "/index.html",
-			//                                "/ws/**",
-			//                                "/swagger/**",
-			//                                "swagger-ui/**",
-			//                                "/api-docs/**",
-			//                                "/signup.html",
-			//                                "/api/v1/reissue"
-			//                        ).permitAll()
-			//                        .anyRequest().authenticated()
-			//                )
 			// 로그아웃 성공 시 / 주소로 이동
-			.logout((logoutConfig) -> logoutConfig.logoutSuccessUrl("/"))
-			.oauth2Login(oauth2 -> oauth2
+//			.logout((logoutConfig) -> logoutConfig.logoutSuccessUrl("/"))
+			.oauth2Login(oauth2Login -> oauth2Login
 				.userInfoEndpoint(endpoint -> endpoint
 					.userService(customOAuth2UserService))
 				.successHandler(oAuth2LoginSuccessHandler) // 2.
 				.failureHandler(oAuth2LoginFailureHandler) // 3.
 			);
-		//                .exceptionHandling(authenticationManager ->authenticationManager
-		//                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-		//                        .accessDeniedHandler(jwtAccessDeniedHandler)
-		//                )
-		//                .addFilterBefore(new JwtAuthenticationFilter(jwtService, userRepository), UsernamePasswordAuthenticationFilter.class);
-
+//			.exceptionHandling(authenticationManager ->authenticationManager
+//					.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+//					.accessDeniedHandler(jwtAccessDeniedHandler)
+//			)
 		return http.build();
 	}
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration config = new CorsConfiguration();
 
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of(
-			"http://localhost:3000",
-			// "http://localhost:9100",
-			// "http://localhost:9090",
-			"http://localhost:5173",
-			"file://",
-			"http://localhost"
-			//"https://rexume.site",
-			//"http://rexume.site"
-		));
-		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-		config.setAllowedHeaders(List.of("*"));
-		config.setExposedHeaders(List.of("*"));
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-		return source;
-	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(); // BCrypt 암호화 방식 사용
+	}
+
+	@Bean
+	public RedirectStrategy redirectStrategy() {
+		return new DefaultRedirectStrategy(); // 기본 리다이렉트 전략 사용
 	}
 }
