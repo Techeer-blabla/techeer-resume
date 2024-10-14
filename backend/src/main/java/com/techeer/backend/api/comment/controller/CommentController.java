92511ae53b517f9a5ae59e4c87524e9464e63e1f
Gwanghyeon-k <hsys06207@gmail.com>
2024-10-14 15:17:30 +0900

    be/feat: 댓글 조회, 이력서 ID로 조회 구현 완료

--- backend/src/main/java/com/techeer/backend/api/comment/controller/CommentController.java
+++ backend/src/main/java/com/techeer/backend/api/comment/controller/CommentController.java
@@ -3,12 +3,15 @@ package com.techeer.backend.api.comment.controller;
 import com.techeer.backend.api.comment.dto.CommentCreateRequest;
 import com.techeer.backend.api.comment.dto.CommentResponse;
 import com.techeer.backend.api.comment.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name="댓글 등록 API")
 @RestController
@@ -18,13 +21,29 @@ public class CommentController {
 
   private final CommentService commentService;
 
-  @PostMapping("/{resume_Id}/comments")
+  @PostMapping("/{resume_id}/comments")
   public ResponseEntity<CommentResponse> createComment(
-      @PathVariable("resume_Id") Long resumeId,
+      @PathVariable("resume_id") Long resumeId,
       @Valid @RequestBody CommentCreateRequest commentRequest) {
 
     CommentResponse commentResponse = commentService.createComment(resumeId, commentRequest);
 
     return new ResponseEntity<>(commentResponse, HttpStatus.CREATED);
+  }
+
+  //단일 댓글 조회
+  @Operation(summary="단일 댓글 조회", description = "하나의 댓글만 조회하는 기능")
+  @GetMapping("/{resume_Id}/comments/{comment_id}")
+  public ResponseEntity<CommentResponse> getCommentById(@PathVariable Long comment_id){
+    CommentResponse response = commentService.getCommentById(comment_id);
+    return ResponseEntity.ok(response);
+  }
+
+  // 이력서 관련 댓글 조회
+  @Operation(summary = "이력서 관련 댓글 조회", description = "이력서에 작성되어 있는 모든 댓글을 조회하는 기능")
+  @GetMapping("/{resume_id}/comments")
+  public ResponseEntity<List<CommentResponse>> getCommentByResumeId(@PathVariable Long resume_id) {
+    List<CommentResponse> responses = commentService.getCommentByResumeId(resume_id);
+    return ResponseEntity.ok(responses);
   }
 }
--- backend/src/main/java/com/techeer/backend/api/comment/service/CommentService.java
+++ backend/src/main/java/com/techeer/backend/api/comment/service/CommentService.java
@@ -7,16 +7,19 @@ import com.techeer.backend.api.comment.dto.CommentResponse;
 import com.techeer.backend.api.comment.repository.CommentRepository;
 import com.techeer.backend.api.resume.domain.Resume;
 import com.techeer.backend.api.resume.repository.ResumeRepository;
-import jakarta.persistence.EntityNotFoundException;
+import com.techeer.backend.global.error.exception.comment.CommentNotFoundException;
+import com.techeer.backend.global.error.exception.comment.NoCommentsForResumeException;
+import com.techeer.backend.global.error.exception.resume.ResumeNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-
-  private static final String RESUME_NOT_FOUND_MESSAGE = "Resume not found";
 
   private final CommentRepository commentRepository;
   private final ResumeRepository resumeRepository;
@@ -24,11 +27,32 @@ public class CommentService {
   @Transactional
   public CommentResponse createComment(Long resumeId, CommentCreateRequest commentCreateRequest){
     Resume resume = resumeRepository.findByIdAndDeletedAtIsNull(resumeId)
-        .orElseThrow(() -> new EntityNotFoundException(RESUME_NOT_FOUND_MESSAGE));
+        .orElseThrow(ResumeNotFoundException::new);
 
     Comment comment = Comment.of(resume, commentCreateRequest);
     commentRepository.save(comment);
 
     return CommentConverter.of(resume, comment);
+  }
+  @Transactional
+  public CommentResponse getCommentById(Long commentId){
+    Comment comment = commentRepository.findById(commentId)
+        .orElseThrow(CommentNotFoundException::new);
+
+    return CommentConverter.of(comment.getResume(), comment);
+  }
+  @Transactional
+  public List<CommentResponse> getCommentByResumeId(Long resumeId) {
+    Resume resume = resumeRepository.findByIdAndDeletedAtIsNull(resumeId)
+        .orElseThrow(ResumeNotFoundException::new);
+    List<Comment> comments = commentRepository.findByResumeId(resumeId);
+
+    if(comments.isEmpty()){
+      throw new NoCommentsForResumeException();
+    }
+
+    return comments.stream()
+        .map(comment -> CommentConverter.of(resume, comment))
+        .collect(Collectors.toList());
   }
 }
