e3ed3e11d2e74d6da50ff678119858b60dec81cf
Gwanghyeon-k <hsys06207@gmail.com>
2024-11-04 16:39:40 +0900

    feat: converter 로직 수정

--- backend/src/main/java/com/techeer/backend/api/comment/converter/CommentConverter.java
+++ backend/src/main/java/com/techeer/backend/api/comment/converter/CommentConverter.java
@@ -1,16 +1,24 @@
 package com.techeer.backend.api.comment.converter;
 
 import com.techeer.backend.api.comment.domain.Comment;
+import com.techeer.backend.api.comment.dto.CommentCreateRequest;
 import com.techeer.backend.api.comment.dto.CommentResponse;
 import com.techeer.backend.api.resume.domain.Resume;
 
 public class CommentConverter {
 
-  public static CommentResponse toCommentResponse(Resume resume, Comment comment) {
-    return CommentResponse.builder()
-        .commentId(comment.getId())
-        .resumeId(resume.getId())
-        .content(comment.getContent())
-        .build();
-  }
+    public static CommentResponse toCommentResponse(Resume resume, Comment comment) {
+        return CommentResponse.builder()
+                .commentId(comment.getId())
+                .resumeId(resume.getId())
+                .content(comment.getContent())
+                .build();
+    }
+
+    public static Comment toComment(Resume resume, CommentCreateRequest commentCreateRequest) {
+        return Comment.builder()
+                .resume(resume)
+                .content(commentCreateRequest.getContent())
+                .build();
+    }
 }
--- backend/src/main/java/com/techeer/backend/api/comment/domain/Comment.java
+++ backend/src/main/java/com/techeer/backend/api/comment/domain/Comment.java
@@ -1,44 +1,45 @@
 package com.techeer.backend.api.comment.domain;
 
-import com.techeer.backend.api.comment.dto.CommentCreateRequest;
 import com.techeer.backend.api.resume.domain.Resume;
 import com.techeer.backend.global.common.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Entity
-@Table(name="댓글")
+@Table(name = "댓글")
 @Builder
 @AllArgsConstructor
 public class Comment extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "comment_id")
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
 
-  @Column(nullable = false, length = 255)
-  private String content;
+    @Column(nullable = false, length = 255)
+    private String content;
 
 
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "resume_id", nullable = false)
-  private Resume resume;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "resume_id", nullable = false)
+    private Resume resume;
 
-  protected Comment() {
-  }
-  // 댓글 내용 수정
-  public void updateContent(String content){
-    this.content = content;
-  }
+    protected Comment() {
+    }
 
-  public static Comment of(Resume resume, CommentCreateRequest commentCreateRequest) {
-    return Comment.builder()
-        .resume(resume)
-        .content(commentCreateRequest.getContent())
-        .build();
-  }
+    // 댓글 내용 수정
+    public void updateContent(String content) {
+        this.content = content;
+    }
 }
