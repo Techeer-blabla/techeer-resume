57d79dcca05ca3241a7d6ca1e9d22fcaca94dd0c
Gwanghyeon-k <hsys06207@gmail.com>
2024-11-04 17:11:40 +0900

    feat: controller 오류 수정

--- backend/src/main/java/com/techeer/backend/api/comment/service/CommentService.java
+++ backend/src/main/java/com/techeer/backend/api/comment/service/CommentService.java
@@ -11,68 +11,67 @@ import com.techeer.backend.global.error.exception.comment.CommentNotFoundExcepti
 import com.techeer.backend.global.error.exception.comment.NoCommentsForResumeException;
 import com.techeer.backend.global.error.exception.resume.ResumeNotFoundException;
 import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
-  private final CommentRepository commentRepository;
-  private final ResumeRepository resumeRepository;
+    private final CommentRepository commentRepository;
+    private final ResumeRepository resumeRepository;
 
-  @Transactional
-  public CommentResponse createComment(Long resumeId, CommentCreateRequest commentCreateRequest){
-    Resume resume = resumeRepository.findByIdAndDeletedAtIsNull(resumeId)
-        .orElseThrow(ResumeNotFoundException::new);
+    @Transactional
+    public CommentResponse createComment(Long resumeId, CommentCreateRequest commentCreateRequest) {
+        Resume resume = resumeRepository.findByIdAndDeletedAtIsNull(resumeId)
+                .orElseThrow(ResumeNotFoundException::new);
 
-    Comment comment = Comment.of(resume, commentCreateRequest);
-    commentRepository.save(comment);
+        Comment comment = CommentConverter.toComment(resume, commentCreateRequest);
+        commentRepository.save(comment);
 
-    return CommentConverter.toCommentResponse(resume, comment);
-  }
-
-  @Transactional
-  public CommentResponse getCommentById(Long commentId){
-    Comment comment = commentRepository.findById(commentId)
-        .orElseThrow(CommentNotFoundException::new);
-
-    return CommentConverter.toCommentResponse(comment.getResume(), comment);
-  }
-
-  @Transactional
-  public List<CommentResponse> getCommentByResumeId(Long resumeId) {
-    Resume resume = resumeRepository.findByIdAndDeletedAtIsNull(resumeId)
-        .orElseThrow(ResumeNotFoundException::new);
-    List<Comment> comments = commentRepository.findByResumeId(resumeId);
-
-    if(comments.isEmpty()){
-      throw new NoCommentsForResumeException();
+        return CommentConverter.toCommentResponse(resume, comment);
     }
 
-    return comments.stream()
-        .map(comment -> CommentConverter.toCommentResponse(resume, comment))
-        .collect(Collectors.toList());
-  }
+    @Transactional
+    public CommentResponse getCommentById(Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(CommentNotFoundException::new);
 
-  @Transactional
-  public void deleteComment(Long comment_id){
-    Comment comment = commentRepository.findById(comment_id)
-        .orElseThrow(CommentNotFoundException::new);
+        return CommentConverter.toCommentResponse(comment.getResume(), comment);
+    }
 
-    commentRepository.delete(comment);
-  }
+    @Transactional
+    public List<CommentResponse> getCommentByResumeId(Long resumeId) {
+        Resume resume = resumeRepository.findByIdAndDeletedAtIsNull(resumeId)
+                .orElseThrow(ResumeNotFoundException::new);
+        List<Comment> comments = commentRepository.findByResumeId(resumeId);
 
-  @Transactional
-  public CommentResponse updateComment(Long comment_id, CommentCreateRequest request){
-    Comment comment = commentRepository.findById(comment_id)
-        .orElseThrow(CommentNotFoundException::new);
+        if (comments.isEmpty()) {
+            throw new NoCommentsForResumeException();
+        }
 
-    comment.updateContent(request.getContent());
+        return comments.stream()
+                .map(comment -> CommentConverter.toCommentResponse(resume, comment))
+                .collect(Collectors.toList());
+    }
 
-    return CommentConverter.toCommentResponse(comment.getResume(), comment);
-  }
+    @Transactional
+    public void deleteComment(Long comment_id) {
+        Comment comment = commentRepository.findById(comment_id)
+                .orElseThrow(CommentNotFoundException::new);
+
+        commentRepository.delete(comment);
+    }
+
+    @Transactional
+    public CommentResponse updateComment(Long comment_id, CommentCreateRequest request) {
+        Comment comment = commentRepository.findById(comment_id)
+                .orElseThrow(CommentNotFoundException::new);
+
+        comment.updateContent(request.getContent());
+
+        return CommentConverter.toCommentResponse(comment.getResume(), comment);
+    }
 }
