6c4ffc24e9a6e34acc6e9c367a279ff340830b3c
sanghun <vitash1215@gmail.com>
2024-11-06 17:23:57 +0900

    feat(json) : swagger json 안 뜨는 문제 해결
    swagger에서 json으로 값이 안 뜨는 문제를 해결

---
+++ backend/src/main/java/com/techeer/backend/global/config/ModelResolverConfig.java
@@ -0,0 +1,18 @@
+package com.techeer.backend.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ModelResolverConfig {
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        return new ModelResolver(objectMapper);
+    }
+}
