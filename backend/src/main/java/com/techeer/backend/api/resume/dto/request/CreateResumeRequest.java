136cc7fdfb59559107df70f8710b8cd8bd626776
sanghun <vitash1215@gmail.com>
2024-11-06 17:53:17 +0900

    fix(dto) : dto naming 및 bug fix
    dto에 entity 삽입 문제와 dto naming을 수정함으로써 에러를 수정하고 명확한 dto 인지 가능

--- backend/src/main/java/com/techeer/backend/api/resume/dto/request/CreateResumeRequest.java
+++ backend/src/main/java/com/techeer/backend/api/resume/dto/request/CreateResumeRequest.java
@@ -1,35 +1,26 @@
 package com.techeer.backend.api.resume.dto.request;
 
-import java.util.List;
-
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.techeer.backend.api.resume.domain.Position;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
+import java.util.List;
 import lombok.Getter;
 
 //todo Getter 한번만 사용하기
 @Getter
-@Builder
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateResumeRequest {
-	// 나중에 로그인 생기면 수정해야 된다.
+    // 나중에 로그인 생기면 수정해야 된다.
+    @NotBlank(message = "username은 필수입니다.")
+    private String username;
 
-	@NotBlank(message = "content는 필수입니다.")
-	private String username;
+    @NotNull(message = "position는 필수입니다.")
+    private String position;
 
-	@NotNull(message = "content는 필수입니다.")
-	private Position position;
+    @NotNull(message = "career는 필수입니다.")
+    private int career;
 
-	@NotNull(message = "content는 필수입니다.")
-	private int career;
+    private List<String> companyNames;
 
-	private List<String> applyingCompanies;
-
-	private List<String> techStacks;
+    private List<String> techStackNames;
 
 }
 
