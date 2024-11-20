5119a2a0c7569958d287207c13321113ec27beec
Yujin Jung <154859664+jung2941@users.noreply.github.com>
2024-11-20 15:00:44 +0900

    [feat] 소셜로그인 기능 연동 (#139)
    * feat: 사용자 프로필 설정
    
    * feat: 로그인 페이지 리디렉션
    
    * Update Token.tsx
    
    * chore: auto-format code [skip ci]
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import MainPage from "./pages/MainPage";
 import ResumeFeedbackPage from "./pages/ResumeFeedbackPage";
 import SearchPage from "./pages/SearchPage";
 import Upload from "./pages/Upload";
+import Login from "./pages/LoginPage";
+import ProtectedRoute from "./utils/Token";
 import MyInfoPage from "./pages/MyInfoPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -17,10 +19,14 @@ export default function App() {
           <div className="flex-grow">
             <Routes>
               <Route path="/" element={<MainPage />} />
-              <Route path="/feedback" element={<ResumeFeedbackPage />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/search" element={<SearchPage />} />
-              <Route path="/upload" element={<Upload />} />
-              <Route path="/myInfo" element={<MyInfoPage />} />
+
+              <Route element={<ProtectedRoute />}>
+                <Route path="/feedback" element={<ResumeFeedbackPage />} />
+                <Route path="/upload" element={<Upload />} />
+                <Route path="/myInfo" element={<MyInfoPage />} />
+              </Route>
             </Routes>
           </div>
         </div>
