d2afb8f26abbd86163851233d080a1a3f4dea65b
GitHub Action <action@github.com>
2024-11-27 15:52:52 +0000

    chore: auto-format code [skip ci]

--- frontend/src/__test__/comment/CommentCard.test.tsx
+++ frontend/src/__test__/comment/CommentCard.test.tsx
@@ -2,11 +2,10 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { vi, describe, it, expect} from "vitest";
+import { vi, describe, it, expect } from "vitest";
 import CommentCard from "../../components/comment/CommentCard";
 
 describe("일반 코멘트 카드 테스트", () => {
-
   const mockComment = {
     id: 1,
     text: "임시 댓글",
@@ -23,7 +22,11 @@ describe("일반 코멘트 카드 테스트", () => {
 
     //When : 댓글이 렌더링 되어있을 때
     render(
-      <CommentCard {...mockComment} onDelete={mockOnDelete} onEdit={mockOnEdit} />
+      <CommentCard
+        {...mockComment}
+        onDelete={mockOnDelete}
+        onEdit={mockOnEdit}
+      />
     );
     //Then : 댓글이 렌더링 되어있다.
     expect(screen.getByText("임시 댓글")).toBeInTheDocument();
@@ -32,12 +35,16 @@ describe("일반 코멘트 카드 테스트", () => {
   it("삭제 버튼 작동확인", () => {
     //Given : 사용자가 댓글을 삭제하고 싶은 상횡
     render(
-      <CommentCard {...mockComment} onDelete={mockOnDelete} onEdit={mockOnEdit} />
+      <CommentCard
+        {...mockComment}
+        onDelete={mockOnDelete}
+        onEdit={mockOnEdit}
+      />
     );
 
     //When : 삭제 버튼을 클릭했을 때
     fireEvent.click(screen.getByText("삭제"));
-    
+
     //Then : onDelete 함수가 호출되어야 한다.
     expect(mockOnDelete).toHaveBeenCalledWith(mockComment.id);
   });
@@ -45,7 +52,11 @@ describe("일반 코멘트 카드 테스트", () => {
   it("수정 버튼 작동 확인(handleEditToggle)", () => {
     //Given : 사용자가 댓글을 수정하고 싶은 상황
     render(
-      <CommentCard {...mockComment} onDelete={mockOnDelete} onEdit={mockOnEdit} />
+      <CommentCard
+        {...mockComment}
+        onDelete={mockOnDelete}
+        onEdit={mockOnEdit}
+      />
     );
     //When : 수정 버튼을 클릭했을 때
     fireEvent.click(screen.getByText("수정"));
@@ -56,7 +67,11 @@ describe("일반 코멘트 카드 테스트", () => {
   it("저장 버튼 작동 확인", () => {
     //Given : 사용자가 댓글을 수정하고 저장하고 싶은 상황
     render(
-      <CommentCard {...mockComment} onDelete={mockOnDelete} onEdit={mockOnEdit} />
+      <CommentCard
+        {...mockComment}
+        onDelete={mockOnDelete}
+        onEdit={mockOnEdit}
+      />
     );
     //When : 수정 버튼을 클릭하고 새로운 내용을 입력하고 저장 버튼을 클릭했을 때
     fireEvent.click(screen.getByText("수정"));
@@ -70,7 +85,11 @@ describe("일반 코멘트 카드 테스트", () => {
   it("수정 취소 버튼 작동 확인", () => {
     //Given : 사용자가 댓글을 수정하고 취소하고 싶은 상황
     render(
-      <CommentCard {...mockComment} onDelete={mockOnDelete} onEdit={mockOnEdit} />
+      <CommentCard
+        {...mockComment}
+        onDelete={mockOnDelete}
+        onEdit={mockOnEdit}
+      />
     );
     const toggleButton = screen.getByTestId("EditButton");
 
@@ -81,9 +100,11 @@ describe("일반 코멘트 카드 테스트", () => {
     fireEvent.click(toggleButton);
 
     //Then : 수정할 수 있는 input이 사라져야 한다.
-    expect(screen.queryByDisplayValue("Updated comment")).not.toBeInTheDocument();
+    expect(
+      screen.queryByDisplayValue("Updated comment")
+    ).not.toBeInTheDocument();
   });
-}); 
+});
 
 describe("수정된 코멘트 카드 테스트", () => {
   const mockComment = {
@@ -101,9 +122,15 @@ describe("수정된 코멘트 카드 테스트", () => {
 
     //When : 댓글이 렌더링 되어있을 때
     render(
-      <CommentCard {...mockComment} onDelete={mockOnDelete} onEdit={mockOnEdit} />
+      <CommentCard
+        {...mockComment}
+        onDelete={mockOnDelete}
+        onEdit={mockOnEdit}
+      />
     );
     //Then : 댓글이 렌더링 되어있다.
-    expect(screen.getByText((content) => content.includes("(수정됨)"))).toBeInTheDocument();
+    expect(
+      screen.getByText((content) => content.includes("(수정됨)"))
+    ).toBeInTheDocument();
   });
-}); 
+});
