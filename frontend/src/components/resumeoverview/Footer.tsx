bec90f419bea68b4a084b6884ff47fa45685b6e1
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-12-18 16:27:31 +0900

    feat : 페이지 이동 임시 구현

--- frontend/src/components/resumeoverview/Footer.tsx
+++ frontend/src/components/resumeoverview/Footer.tsx
@@ -1,20 +1,37 @@
 import chevronLeft from "../../assets/chevron-left.png";
 import chevronRight from "../../assets/chevron-right.png";
 
-function Footer() {
+interface FooterProps {
+  laterResumeId: number | null;
+  previousResumeId: number | null;
+}
+
+function Footer({
+  laterResumeId,
+  previousResumeId,
+}: FooterProps): React.ReactElement {
+
+  const handlePreviousResume = () => {
+    console.log("Previous Resume");
+  }
+
+  const handleNextResume = () => {
+    console.log("Next Resume");
+  }
+
   return (
     <footer className="w-full h-[49px] bg-white border-t border-[#e0e0e0] flex justify-between items-center px-4">
       {/* Previous Resume */}
-      <div className="flex items-center space-x-2 mt-3 -mb-5">
+      <div onClick={handlePreviousResume} className="flex items-center space-x-2 mt-3 -mb-5">
         <img src={chevronLeft} alt="Previous Resume" className="w-6 h-6" />{" "}
         {/* Left Chevron */}
-        <div className="text-[#6e6e6e] text-[15px] font-medium font-pretendard">
+        <div  className="text-[#6e6e6e] text-[15px] font-medium font-pretendard">
           이전 이력서
         </div>
       </div>
 
       {/* Next Resume */}
-      <div className="flex items-center space-x-2 mt-3 -mb-5">
+      <div onClick={handleNextResume} className="flex items-center space-x-2 mt-3 -mb-5">
         <div className="text-[#6e6e6e] text-[15px] font-medium font-pretendard">
           다음 이력서
         </div>
