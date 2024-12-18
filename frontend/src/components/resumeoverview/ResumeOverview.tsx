eb822757e5fe54ba2e9e70afacf277cc95f7e145
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-12-18 16:24:55 +0900

    feat : 불필요한 API 호출 및 상태관리 제거

--- frontend/src/components/resumeoverview/ResumeOverview.tsx
+++ frontend/src/components/resumeoverview/ResumeOverview.tsx
@@ -1,10 +1,33 @@
 import ResumeOverviewSection from "./ResumeOverviewSection.tsx";
 
-function ResumeOverview() {
+interface ResumeOverviewProps {
+  userName: string;
+  position: string;
+  career: number;
+  techStackNames: string[];
+  fileUrl: string;
+  isLoading: boolean;
+}
+
+function ResumeOverview({
+  userName,
+  position,
+  career,
+  techStackNames,
+  fileUrl,
+  isLoading,
+}: ResumeOverviewProps): React.ReactElement {
   return (
     <div className="w-[534px] h-auto  bg-white p-4 z-1">
       {/* Personal Info Section */}
-      <ResumeOverviewSection />
+      <ResumeOverviewSection
+        userName={userName}
+        position={position}
+        career={career}
+        techStackNames={techStackNames}
+        fileUrl={fileUrl}
+        isLoading={isLoading}
+      />
     </div>
   );
 }
