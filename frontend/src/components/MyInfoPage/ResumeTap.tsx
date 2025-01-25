85a43b7150ae8b40983da94b25c6e30a0347edd5
jung2941 <ujin2941@naver.com>
2025-01-24 16:40:20 -0800

    refactor: 불필요한 eslint 규칙 제거 및 axios 임포트 경로 수정

--- frontend/src/components/MyInfoPage/ResumeTap.tsx
+++ frontend/src/components/MyInfoPage/ResumeTap.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "../../utils/axiosInstance.ts";
+import axios from "axios";
 import ResumeItem from "../../components/MyInfoPage/ResumeItem.tsx";
 import { ResumeType } from "../../dataType.ts";
 
