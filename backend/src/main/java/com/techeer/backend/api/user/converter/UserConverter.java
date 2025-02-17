2ec0e41566fb0d31d2fbaab46490edbfbcdc3e67
sanghun <vitash1215@gmail.com>
2025-02-17 22:44:01 +0900

    fix : 사용되지 않는 코드 삭제

--- backend/src/main/java/com/techeer/backend/api/user/converter/UserConverter.java
+++ backend/src/main/java/com/techeer/backend/api/user/converter/UserConverter.java
@@ -1,21 +1,9 @@
 package com.techeer.backend.api.user.converter;
 
-import com.techeer.backend.api.user.domain.Role;
-import com.techeer.backend.api.user.domain.SocialType;
 import com.techeer.backend.api.user.domain.User;
 import com.techeer.backend.api.user.dto.response.UserInfoResponse;
 
 public class UserConverter {
-    public static User fromSignUp(String email, String username, String refreshToken, Role role,
-                                  SocialType socialType) {
-        return User.builder()
-                .email(email)
-                .username(username)
-                .refreshToken(refreshToken)
-                .role(role)
-                .socialType(socialType)
-                .build();
-    }
 
     public static UserInfoResponse ofUserInfoResponse(User user) {
         return UserInfoResponse.builder()
